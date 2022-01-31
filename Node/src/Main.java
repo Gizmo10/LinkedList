d464a36d5fc2053478844e8417bce884db9727b0
Gizmo10 <sw.ndzala@gmail.com>
2022-01-30 17:06:34 -0900

    removeDuplicates method done and tested

--- Node/src/Main.java
+++ Node/src/Main.java
@@ -2,5 +2,21 @@ public class Main {
 
     public static void main(String [] args){
 
+        Node head = new Node(0);
+        int [] arr = {2,2,2,3,4,1,3,4,5,6,6,6};
+
+        for(int num: arr){
+
+            head.appendToEnd(num);
+        }
+
+       head = head.removeDuplicates();
+
+        while(head != null){
+
+            System.out.print(head.data);
+            head = head.next;
+        }
+
     }
 }
