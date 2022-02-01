290214345948db9b09bc158efec2813eacdabe4a
Gizmo10 <sw.ndzala@gmail.com>
2022-01-31 15:30:20 -0900

    Added test for sumList

--- Node/src/Main.java
+++ Node/src/Main.java
@@ -2,15 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Node head = new Node();
-        int[] arr = {1,4,5,2,7,3,6,8,4,9};
+        Node list1 = new Node();
+        Node list2 = new Node();
+        int[] arr = {1,0,6,1};
+        int[] arr2 = {5,9,2};
 
         for (int num : arr) {
 
-            head.appendToEnd(num);
+            list1.appendToEnd(num);
         }
 
-      Node temp = head.partition(5);
+        for(int num : arr2){
+
+            list2.appendToEnd(num);
+        }
+
+      Node temp = list1.sumLists(list2);
 
         while (temp != null) {
 
